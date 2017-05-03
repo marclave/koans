8b31f8cc58a211080cfa7ad9249f455145b7d7d0
marclave <marclave@uvic.ca>
2017-05-03 14:24:53 -0700

    Finished dice koan

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -5,6 +5,26 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # class DiceSet
 #   code ...
 # end
+class DiceSet
+  attr_reader :values
+  def initialize()
+    @values = []
+  end
+
+  def roll(diceToRoll)
+    @values = []
+    diceToRoll.times { @values << rolldie }
+  end
+
+  def rolldie
+    generateRandomNumber(1, 6)
+  end
+
+  def generateRandomNumber(min, max)
+    rand(1..6)
+  end
+
+end
 
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
